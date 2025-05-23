a27348c07666c692440bb98d44af8f3fb962687a
Henrique Kachutski <henrique_kachutski@MacBook-Air-de-Henrique.local>
2025-05-22 21:59:45 -0300

    Client tests finished

--- tests/Unit/Models/Users/ClientTest.php
+++ tests/Unit/Models/Users/ClientTest.php
@@ -6,6 +6,9 @@ use App\Models\Client;
 use App\Models\User;
 use Tests\TestCase;
 
+use function PHPUnit\Framework\assertEquals;
+use function PHPUnit\Framework\equalTo;
+
 class ClientTest extends TestCase{
    private User $user_client;
    private Client $client;
@@ -13,18 +16,18 @@ class ClientTest extends TestCase{
 
    public function setUp(): void
    {
+      parent::setUp();
+
       $this->user_client = new User([
          'name' => 'User_client',
-         'email' => 'artist@example.com',
+         'email' => 'user@example.com',
          'password' => '123456',
          'password_confirmation' => '123456'
       ]);
       $this->user_client->save();
 
       $this->client = new Client([
-        'bio' => 'me siga',
-        'portfolio_url' => 'hhtps://teste',
-        'ai_detection_count' => 0,
+        'phone' => '42999997826',
         'user_id' => $this->user_client->id
       ]);
       $this->client->save();
@@ -35,19 +38,51 @@ class ClientTest extends TestCase{
       $this->assertCount(1, Client::all());
       $this->assertEquals($this->user_client->id, $this->client->user_id);
    }
-/* 
+
    public function test_should_return_all_clients(): void 
    {
-      $newClient = new Client([
-         'phone' => '42996752016',
-         'user_id' => $this->user2->id
+      $user_client_2 = new User([
+         'name' => 'User_client2',
+         'email' => 'user2@example.com',
+         'password' => '123456',
+         'password_confirmation' => '123456'
       ]);
-      $newClient->save();
+      $user_client_2->save();
+
+      $client_2 = new Client([
+         'phone' => '42996752016',
+         'user_id' => $user_client_2->id
+      ]);
+      $client_2->save();
 
       
       $this->assertCount(2, Client::all());
    }
 
+   public function test_should_validate_client_is_null(): void {
+      $client = new Client([
+         'phone' => '42996752016',
+      ]);
+      $client->save();
+
+      $this->assertEquals(0 || null, $client->validates());
+   }
+
+   public function test_should_associate_client_to_user(): void {
+      $user = new User([
+         'name' => 'User_client',
+         'email' => 'userTeste@example.com',
+         'password' => '123456',
+         'password_confirmation' => '123456'
+      ]);
+      $user->save();
+
+      
+      $client = $user->belongsTo('42998247591', $user->id);
+      assertEquals($client ,$user->belongsTo('42998247591', $user->id));
+      
+   }
+/* 
    public function test_destroy_should_remove_client(): void 
    {
       $this->client->destroy();
