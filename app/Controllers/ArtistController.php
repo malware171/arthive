129e296aa7a1d7c1d1b8bcbc8626bb396eb512b0
Henrique Kachutski <henrique_kachutski@MacBook-Air-de-Henrique.local>
2025-05-26 18:17:35 -0300

    Improve styles in new artwork post and start the implementation in artistContoller

--- app/Controllers/ArtistController.php
+++ app/Controllers/ArtistController.php
@@ -5,6 +5,7 @@ namespace App\Controllers;
 use Core\Http\Controllers\Controller;
 use Core\Http\Request;
 use Lib\Authentication\Auth;
+use Lib\FlashMessage;
 
 class ArtistController extends Controller
 {
@@ -17,7 +18,24 @@ class ArtistController extends Controller
     public function new(Request $request): void
     {
         $title = 'Nova Arte';
-        $subtitle = 'Descrição';
-        $this->render('adminPage/new', compact('title', 'subtitle'));
+        $description = 'Descrição';
+        $this->render('adminPage/new', compact('title', 'description'));
+    }
+    public function newArtwork(Request $request): void
+    {
+        $parms = $request->getParam('artwork');
+
+        if(
+            !is_array($parms) ||
+            !isset($parms['title'], $params['description'], $params['image'])
+        ) {
+            FlashMessage::danger('All inputs need to preenchido');
+            $this->redirectTo(route('artist.new'));
+            return;
+        }
+        //  CHAMA O METODO DENTRO DO MODEL
+        // PARA SALVAR A POSTAGEM
+
+
     }
 }
