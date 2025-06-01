256c91115afc68bfddc989bb9a4f550a8602ff12
Diogo Abreu <abreudiogo177@gmail.com>
2025-06-01 16:17:43 -0300

    paginator

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -3,13 +3,18 @@
 namespace App\Controllers;
 
 use App\Models\Artwork;
+use Core\Http\Request;
 use Core\Http\Controllers\Controller;
 
 class HomeController extends Controller
 {
-    public function index(): void
+    public function index(Request $request): void
     {
         $artworks = Artwork::all();
+        $paginator = Artwork::paginate(
+            page: $request->getParam('page', 1),
+            per_page: 10,
+        );
 
         $title = 'Todas as obras';
         $this->render('home/index', compact('title', 'artworks'));
