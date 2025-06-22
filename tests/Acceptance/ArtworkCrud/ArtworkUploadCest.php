f0062b4aef6bc808d4ced7d123befca1c67a496a
Diogo Abreu <abreudiogo177@gmail.com>
2025-06-21 22:09:09 -0300

    Added successful upload test, and zip extension in dockerfile

--- tests/Acceptance/ArtworkCrud/ArtworkUploadCest.php
+++ tests/Acceptance/ArtworkCrud/ArtworkUploadCest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Acceptance\ArtworkCrud;
 
+
 use Tests\Acceptance\BaseAcceptanceCest;
 use Tests\Support\AcceptanceTester;
 use App\Models\User;
@@ -18,6 +19,30 @@ class ArtworkUploadCest extends BaseAcceptanceCest
         $I->see('Crie seu post');
     }
 
+    public function tryUploadWithValidData(AcceptanceTester $I): void
+    {
+        $this->loginArtist($I);
+
+        $category = new \App\Models\Category([
+            'name' => 'Categoria Teste'
+        ]);
+        $category->save();;
+
+        $I->amOnPage('/admin/artworks/new');
+        $I->see('Crie seu post');
+
+        $I->fillField('artwork[title]', 'Minha Arte Teste');
+        $I->fillField('artwork[description]', 'Descrição da arte teste');
+
+        $I->selectOption('artwork[category_id]', $category->id);
+
+        $I->attachFile('image', 'test-image.jpg');
+
+        $I->click('Publicar post');
+
+        $I->see('Sucesso');
+        $I->seeInCurrentUrl('/admin/artworks');
+    }
 
     public function loginArtist(AcceptanceTester $page): void
     {
